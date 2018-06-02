81c6e096bbb96f219b8005d8dd3557077b0b22fa
Krzysztof Czarnecki <czarnecki.krzysiek@gmail.com>
2018-06-02 09:50:00 +0200

    + screen parent class

--- screen.py
+++ screen.py
@@ -10,7 +10,48 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+gi.require_version('GLib', '2.0')
+from gi.repository import GLib
+
+gi.require_version('GdkPixbuf', '2.0')
+from gi.repository import GdkPixbuf as Gpb
+
 class Screen(Gtk.Window):
     def __init__(self, title, width, height):
-        super(TestScreen, self).__init__(title=title)
+        super(Screen, self).__init__(title=title)
 
+        self.height = int(height)
+        if self.height <= 0:
+            raise ValueError("height")
+        
+        self.width = int(width)
+        if self.width <= 0:
+            raise ValueError("width")
+        
+        fix = Gtk.Fixed()
+        self.add(fix)
+        
+        ebox = Gtk.EventBox()
+        ebox.connect ('button-press-event',
+                      self.on_clicked_mouse)
+        fix.put(ebox, 0,0)
+        
+        self.img = Gtk.Image()
+        ebox.add(self.img)
+        self.show_all()
+
+    def screen_refresh(self, screen):
+        tmp = GLib.Bytes.new(screen)
+        rgbf = Gpb.Colorspace.RGB
+        
+        w = self.width 
+        h = self.height
+        w3 = 3 * self.width
+        args = tmp, rgbf, False, 8, w, h, w3
+        
+        pbuf = Gpb.Pixbuf.new_from_bytes(*args)
+        self.img.set_from_pixbuf(pbuf)
+
+    def on_clicked_mouse (self, box, event):        
+        x, y = int(event.x), int(event.y)
+        print("XY: {} {}".format(x, y))
--- test-screen.py
+++ test-screen.py
@@ -6,52 +6,22 @@
 # brief: displays for signal presentation
 # opensource licence: LGPL-2.1
 
-
 import gi
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-gi.require_version('GLib', '2.0')
-from gi.repository import GLib
-
-gi.require_version('GdkPixbuf', '2.0')
-from gi.repository import GdkPixbuf as Gpb
-
+from screen import Screen
 from random import randint
 
 
-class TestScreen(Gtk.Window):
+class TestScreen(Screen):
     def __init__(self, title, width, height):
-        super(TestScreen, self).__init__(title=title)
+        super(TestScreen, self).__init__(title, width, height)
 
-        fix = Gtk.Fixed()
-        self.add(fix)
-        
-        ebox = Gtk.EventBox()
-        ebox.connect ('button-press-event',
-                      self.on_clicked_mouse)
-        fix.put(ebox, 0,0)
-        
-        self.img = Gtk.Image()
-        ebox.add(self.img)
-
-        gen = range(3 * width * height)
+        gen = range(3 * self.width * self.height)
         screen = [randint(0, 255) for n in gen]        
-        self.screen_refresh(screen, width, height)
-        self.show_all()
-
-    def screen_refresh(self, screen, width, height):
-        tmp = GLib.Bytes.new(screen)
-        rgbcode = Gpb.Colorspace.RGB
-        args = tmp, rgbcode, False, 8, width, height, 3*width
-        pbuf = Gpb.Pixbuf.new_from_bytes(*args)
-        self.img.set_from_pixbuf(pbuf)
-        
-    def on_clicked_mouse (self, box, event):        
-        x = int(event.x)
-        y = int(event.y)
-        print("xy: %d %d" % (x, y))
+        self.screen_refresh(screen)
 
 
 win = TestScreen("Test Screen", 400, 300)              
